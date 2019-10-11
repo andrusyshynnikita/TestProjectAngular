981104972201419f5b7ca05c8fc3b9c9818f663e
Dev <Dev>
2019-10-11 09:24:27 +0300

    add product shop

--- TestProjectAngular.UI/src/app/app.module.ts
+++ TestProjectAngular.UI/src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './Modules/core/components/top-bar/top-bar.component';
+import { ProductListComponent } from './Modules/core/components/product-list/product-list.component';
+import {ProductAlertsComponent} from './Modules/core/components/product-alerts/product-alerts.component';
+import {ProductDetailsComponent} from './Modules/core/components/product-details/product-details.component';
+
+import {CartService} from './Modules/core/Services/cart-service/cart.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      {path: 'products/:productId', component: ProductDetailsComponent},
+    ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartService
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
